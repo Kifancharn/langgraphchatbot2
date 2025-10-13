0129263a9ca891ec25c88c976ae30359d24bf1ba
kiran <kirankifan7348@gmail.com>
2025-10-13 10:05:26 +0530

    Update chatbot2.py

--- chatbot2.py
+++ chatbot2.py
@@ -1,6 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 
-#llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro", google_api_key="AIzaSyCBJLl4uJbMEbrHkdyYffjp7jTKP0jZHDQ")
-#result = llm.invoke("Sing a ballad of LangChain.")
-#print(result)
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro", google_api_key="ADD API KEY")
+result = llm.invoke("Sing a ballad of LangChain.")
+
+print(result)
